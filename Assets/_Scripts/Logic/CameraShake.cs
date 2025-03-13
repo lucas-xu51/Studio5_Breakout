e6cf13da222d63ace1305c8d1f9b512b25b16eb0
Lucas Xu <xuhaozhe51@gmail.com>
2025-03-13 13:00:19 -0700

    CameraShake

--- Assets/_Scripts/Logic/CameraShake.cs
+++ Assets/_Scripts/Logic/CameraShake.cs
@@ -1,16 +1,32 @@
+using System.Collections;
 using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static CameraShake Instance;
+    private Vector3 originalPosition;
+
+    void Awake()
     {
-        
+        Instance = this;
+        originalPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator Shake(float duration, float magnitude)
     {
-        
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.position = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.position = originalPosition;
     }
 }
