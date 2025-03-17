4226973045740cb45ed47f83de8b959e811d9c4b
Iverson Yuan <100217991+humb1e1989@users.noreply.github.com>
2025-03-17 13:35:25 +0800

    Solve the scene problem

--- Assets/_Scripts/UI/ButtonHooks.cs
+++ Assets/_Scripts/UI/ButtonHooks.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement; // Ìí¼ÓSceneManagementÃüÃû¿Õ¼ä
 
 public class ButtonHooks : MonoBehaviour
 {
     public void LoadNextScene()
     {
         Debug.Log("click");
-        SceneHandler.Instance.LoadNextScene();
+        //SceneHandler.Instance.LoadNextScene();
+        SceneManager.LoadScene("Level 1");
     }
 
     public void ExitToMenu()
